1b29fd6754b148b6e636ff11f494b35af8f5c9fd
MKettermann <ultramk10@gmail.com>
2023-04-21 12:54:14 -0300

    mkSeletor

--- js/mkSeletor.js
+++ js/mkSeletor.js
@@ -1,9 +1,9 @@
 /*TIMER DE BUSCA: CRIA O DROPDOWN e EVENTO LISTENER*/
 var elemento;
-setTimeout(() => {
+setInterval(() => {
 	document.querySelectorAll("input.mkSeletor").forEach((e) => {
 		if (e.nextElementSibling.classList.contains("mkSeletorExibe")) {
-			console.log("achou");
+			mkSeletorUpdate(e);
 		} else {
 			let ePai = e.parentElement;
 			elemento = e;
@@ -16,13 +16,34 @@ setTimeout(() => {
 			divMkSeletorExibe.style.height = e.offsetHeight + 2 + "px";
 			divMkSeletorBloco.appendChild(e);
 			divMkSeletorBloco.appendChild(divMkSeletorExibe);
+			let divMkSeletorExibeSelecionado = document.createElement("div");
+			divMkSeletorExibeSelecionado.className = "mkSeletorExibeSelecionado";
+			let divMkSeletorExibeFlexinha = document.createElement("div");
+			divMkSeletorExibeFlexinha.className = "mkSeletorExibeFlexinha";
+			divMkSeletorExibeFlexinha.innerHTML =
+				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16"><path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/></svg>';
 
+			divMkSeletorExibe.appendChild(divMkSeletorExibeSelecionado);
+			divMkSeletorExibe.appendChild(divMkSeletorExibeFlexinha);
+
+			mkSeletorUpdate(e);
 			// e.classList.add("mkSecreto");
 		}
-		console.log("Sel Limit: " + e.getAttribute("data-selapenas"));
-		console.log("Array Exibe: " + e.getAttribute("data-seletorarray"));
-		console.log(JSON.parse(e.getAttribute("data-seletorarray")));
 	});
-}, 1000);
+}, 2000);
 
 /* EVENTO: Atualiza lista*/
+const mkSeletorUpdate = (e) => {
+	let selLimit = e.getAttribute("data-selapenas");
+	let seletorArray = JSON.parse(e.getAttribute("data-seletorarray"));
+	let naoAchou = true;
+	seletorArray.forEach((o) => {
+		if (e.value == o.k) {
+			e.nextElementSibling.firstElementChild.innerHTML = o.v;
+			naoAchou = false;
+		}
+	});
+	if (naoAchou) {
+		e.nextElementSibling.firstElementChild.innerHTML = "";
+	}
+};
